3187ca0ba31430a044679a3c58e93145400aa8da
chalasr <robin.chalas@epitech.eu>
2015-11-11 19:27:47 +0100

    optimize UX on interactive mode

--- Command/SetupCommand.php
+++ Command/SetupCommand.php
@@ -3,13 +3,13 @@
 namespace Chalasdev\CapistranoBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
-use Sensio\Bundle\GeneratorBundle\Command\Helper\QuestionHelper;
+
 
 class SetupCommand extends ContainerAwareCommand
 {
@@ -21,38 +21,56 @@ class SetupCommand extends ContainerAwareCommand
       ->setDescription('Setup capistrano deployment configuration in interactive mode');
    }
 
+   protected function execute(InputInterface $input, OutputInterface $output)
+   {
+
+   }
+   
    protected function interact(InputInterface $input, OutputInterface $output)
    {
       $fs = new Filesystem();
-      $helper = $this->getHelper('question');
+      $questionHelper = $this->getHelper('question');
+      $formatter = $this->getHelper('formatter');
+      $style = new OutputFormatterStyle('white', 'blue', array('bold'));
+      $output->getFormatter()->setStyle('title', $style);
       $root = $this->getContainer()->get('kernel')->getRootDir();
       if (false !== $fs->exists("{$root}/../config")) {
           $fs->remove("{$root}/../config");
       }
+      if (false === $fs->exists("{$root}/../Capfile")) {
+          $fs->touch("{$root}/../Capfile");
+          $capfile =
+          "require 'capistrano/setup'".PHP_EOL.
+          "require 'capistrano/deploy'".PHP_EOL.
+          "require 'capistrano/composer'".PHP_EOL.
+          "require 'capistrano/symfony'".PHP_EOL;
+          $fs->dumpFile("{$root}/../Capfile", $capfile);
+      }
+      if (false === $fs->exists("{$root}/../Gemfile")) {
+          $fs->touch("{$root}/../Gemfile");
+          $gems =
+          "gem 'capistrano'".PHP_EOL.
+          "gem 'capistrano-symfony'".PHP_EOL.
+          "gem 'capistrano-rbenv'".PHP_EOL;
+          $fs->dumpFile("{$root}/../Gemfile", $gems);
+      }
+      $welcome = $formatter->formatBlock("Welcome to chalasdev/capistrano", "title", true);
+      $output->writeln(['', $welcome, '', 'This bundle provide automation for your deployment workflow, built on top of <comment>capistrano/symfony</comment> rubygem .', 'Created by Robin Chalas - github.com/chalasr', '']);
+      $output->writeln([$formatter->formatSection('SETUP', 'Project settings'), '']);
       $fs->mirror(
-          $root.'/../vendor/chalasdev/capistrano-bundle/Chalasdev/CapistranoBundle/Resources/config/capistrano', //production
-          // $root.'/../src/Chalasdev/CapistranoBundle/Resources/config/capistrano', //development
-          $root.'/../config/'
+          // "{$root}/../vendor/chalasdev/capistrano-bundle/Chalasdev/CapistranoBundle/Resources/config/capistrano", //production
+          "{$root}/../src/Chalasdev/CapistranoBundle/Resources/config/capistrano", //development
+          "{$root}/../config/"
       );
       $path = $root."/../config/";
       $deployRb = $path.'deploy.rb';
-      $appPath = explode('/', $this->getContainer()->get('kernel')->getRootDir());
+      $appPath = explode('/', $root);
       $appName = $appPath[count($appPath)-2];
-      $productionStaging = [];
-      $data = [
-          'repo_url' => '',
-          'ssh_user' => '',
-          'deploy_to' => '',
-          'branch' => '',
-          'model_manager' => '',
-          'symfony_env' => '',
-          'use_sudo' => '',
-          'keep_releases' => '',
-      ];
+      $data = [];
       $properties = [
           "application" => [
               "helper" => $appName,
-              "label" => "Repository",
+              "label" => "Application",
               "autocomplete" => [$appName]
           ],
           "repo_url" => [
@@ -111,7 +129,7 @@ class SetupCommand extends ContainerAwareCommand
           if (isset($property['autocomplete'])) {
               $question->setAutocompleterValues($property['autocomplete']);
           }
-          $data[$key] = $helper->ask($input, $output, $question);
+          $data[$key] = $questionHelper->ask($input, $output, $question);
       }
       foreach ($data as $k => $v) {
           if (!is_bool($v) && !is_int($v)) {
@@ -125,7 +143,9 @@ class SetupCommand extends ContainerAwareCommand
           }
           file_put_contents($deployRb, $expression, FILE_APPEND);
       }
-      $output->writeln("<comment>deploy.rb succesfully created.</comment>");
+      $output->writeln(['', " > generating <comment>{$appName}/config/deploy.rb</comment>"]);
+      $output->writeln(["<info>Successfully created.</info>", '']);
+      $output->writeln([$formatter->formatSection('PRODUCTION', 'Remote server / SSH settings'), '']);
 
       // Production staging
       $stagingPath = $root."/../config/deploy/production.rb";
@@ -137,12 +157,8 @@ class SetupCommand extends ContainerAwareCommand
               "label" => "Domain name"
           ],
       ];
-      $serverProps = [
-          "domain" => "",
-          "user" => $data["ssh_user"],
-      ];
       $question = new Question("<info>{$serverOptions['domain']['label']}</info> [<comment>{$serverOptions['domain']['helper']}</comment>]: ", $serverOptions['domain']['helper']);
-      $sshProps["domain"] = $helper->ask($input, $output, $question);
+      $sshProps["domain"] = $questionHelper->ask($input, $output, $question);
       $expression = PHP_EOL."server '{$sshProps['domain']}',".PHP_EOL."user: '{$data["ssh_user"]}',".PHP_EOL;
       // Remote Server
       $sshOptions = [
@@ -176,9 +192,9 @@ class SetupCommand extends ContainerAwareCommand
               $question->setAutocompleterValues($property['autocomplete']);
           }
           if (in_array($key, ["auth_methods", "keys"])) {
-              $sshProps[$key] = "%w(".$helper->ask($input, $output, $question).")";
+              $sshProps[$key] = "%w(".$questionHelper->ask($input, $output, $question).")";
           } else {
-              $sshProps[$key] = $helper->ask($input, $output, $question);
+              $sshProps[$key] = $questionHelper->ask($input, $output, $question);
           }
       }
       $expression .= "ssh_options: {".PHP_EOL;
@@ -192,15 +208,6 @@ class SetupCommand extends ContainerAwareCommand
       $expression .= "}";
       file_put_contents($stagingPath, $expression);
 
-      $output->writeln("<comment>Remote server succesfully configured</comment>");
+      $output->writeln("<comment>Remote server successfully configured</comment>");
    }
-
-   protected function execute(InputInterface $input, OutputInterface $output)
-   {
-       $fs = new Filesystem();
-       $helper = $this->getHelper('question');
-       $root = $this->getContainer()->get('kernel')->getRootDir()."/..";
-       $fs->touch("{$root}/Capfile");
-   }
-
 }
