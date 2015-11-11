066cae5b1affe50c9b311afe4fb2621a81fb5eca
chalasr <robin.chalas@epitech.eu>
2015-11-11 20:10:35 +0100

    remove version from composer.json

--- Command/SetupCommand.php
+++ Command/SetupCommand.php
@@ -4,205 +4,202 @@ namespace Chalasdev\CapistranoBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Filesystem\Filesystem;
 
-
 class SetupCommand extends ContainerAwareCommand
 {
-
-   protected function configure()
-   {
-      $this
+    protected function configure()
+    {
+        $this
       ->setName('capistrano:setup')
       ->setDescription('Setup capistrano deployment configuration in interactive mode');
-   }
+    }
 
-   protected function execute(InputInterface $input, OutputInterface $output)
-   {
-   }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
 
-   protected function interact(InputInterface $input, OutputInterface $output)
-   {
-      $fs = new Filesystem();
-      $questionHelper = $this->getHelper('question');
-      $formatter = $this->getHelper('formatter');
-      $style = new OutputFormatterStyle('white', 'blue', array('bold'));
-      $output->getFormatter()->setStyle('title', $style);
-      $root = $this->getContainer()->get('kernel')->getRootDir();
-      if (false !== $fs->exists("{$root}/../config")) {
-          $fs->remove("{$root}/../config");
-      }
-      if (false === $fs->exists("{$root}/../Capfile")) {
-          $fs->touch("{$root}/../Capfile");
-          $capfile =
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $fs = new Filesystem();
+        $questionHelper = $this->getHelper('question');
+        $formatter = $this->getHelper('formatter');
+        $style = new OutputFormatterStyle('white', 'blue', array('bold'));
+        $output->getFormatter()->setStyle('title', $style);
+        $root = $this->getContainer()->get('kernel')->getRootDir();
+        if (false !== $fs->exists("{$root}/../config")) {
+            $fs->remove("{$root}/../config");
+        }
+        if (false === $fs->exists("{$root}/../Capfile")) {
+            $fs->touch("{$root}/../Capfile");
+            $capfile =
           "require 'capistrano/setup'".PHP_EOL.
           "require 'capistrano/deploy'".PHP_EOL.
           "require 'capistrano/composer'".PHP_EOL.
           "require 'capistrano/symfony'".PHP_EOL;
-          $fs->dumpFile("{$root}/../Capfile", $capfile);
-      }
-      if (false === $fs->exists("{$root}/../Gemfile")) {
-          $fs->touch("{$root}/../Gemfile");
-          $gems =
+            $fs->dumpFile("{$root}/../Capfile", $capfile);
+        }
+        if (false === $fs->exists("{$root}/../Gemfile")) {
+            $fs->touch("{$root}/../Gemfile");
+            $gems =
           "gem 'capistrano'".PHP_EOL.
           "gem 'capistrano-symfony'".PHP_EOL.
           "gem 'capistrano-rbenv'".PHP_EOL;
-          $fs->dumpFile("{$root}/../Gemfile", $gems);
-      }
-      $welcome = $formatter->formatBlock("Welcome to chalasdev/capistrano", "title", true);
-      $output->writeln(['', $welcome, '', 'This bundle provide automation for your deployment workflow, built on top of <comment>capistrano/symfony</comment> rubygem .', 'Created by Robin Chalas - github.com/chalasr', '']);
-      $output->writeln([$formatter->formatSection('SETUP', 'Project settings'), '']);
-      $fs->mirror(
+            $fs->dumpFile("{$root}/../Gemfile", $gems);
+        }
+        $welcome = $formatter->formatBlock('Welcome to chalasdev/capistrano', 'title', true);
+        $output->writeln(['', $welcome, '', 'This bundle provide automation for your deployment workflow, built on top of <comment>capistrano/symfony</comment> rubygem .', 'Created by Robin Chalas - github.com/chalasr', '']);
+        $output->writeln([$formatter->formatSection('SETUP', 'Project settings'), '']);
+        $fs->mirror(
           "{$root}/../vendor/chalasdev/capistrano-bundle/Chalasdev/CapistranoBundle/Resources/config/capistrano", //production
           "{$root}/../config/"
       );
-      $path = $root."/../config/";
-      $deployRb = $path.'deploy.rb';
-      $appPath = explode('/', $root);
-      $appName = $appPath[count($appPath)-2];
-      $data = [];
-      $properties = [
-          "application" => [
-              "helper" => $appName,
-              "label" => "Application",
-              "autocomplete" => [$appName]
+        $path = $root.'/../config/';
+        $deployRb = $path.'deploy.rb';
+        $appPath = explode('/', $root);
+        $appName = $appPath[count($appPath) - 2];
+        $data = [];
+        $properties = [
+          'application' => [
+              'helper' => $appName,
+              'label' => 'Application',
+              'autocomplete' => [$appName],
           ],
-          "repo_url" => [
-              "helper" => "git@github.com:{user}/{repo}.git",
-              "label" => "Repository",
-              "autocomplete" => ["git@github.com:chalasr/{$appName}.git", "git@git.sutunam.com/rchalas/{$appName}.git", "git@git.chaladev.fr:chalasr/{$appName}.git"]
+          'repo_url' => [
+              'helper' => 'git@github.com:{user}/{repo}.git',
+              'label' => 'Repository',
+              'autocomplete' => ["git@github.com:chalasr/{$appName}.git", "git@git.sutunam.com/rchalas/{$appName}.git", "git@git.chaladev.fr:chalasr/{$appName}.git"],
           ],
-          "branch" =>  [
-              "helper" => "master",
-              "label" => "Git branch"
+          'branch' => [
+              'helper' => 'master',
+              'label' => 'Git branch',
           ],
-          "ssh_user" => [
-              "helper" => "chalasr",
-              "label" => "SSH username"
+          'ssh_user' => [
+              'helper' => 'chalasr',
+              'label' => 'SSH username',
           ],
-          "deploy_to" => [
-              "helper" => "",
-              "label" => "Remote directory"
+          'deploy_to' => [
+              'helper' => '',
+              'label' => 'Remote directory',
           ],
-          "model_manager" =>  [
-              "helper" => "doctrine",
-              "label" => "Model manager"
+          'model_manager' => [
+              'helper' => 'doctrine',
+              'label' => 'Model manager',
           ],
-          "symfony_env" =>  [
-              "helper" => "prod",
-              "label" => "Environment"
+          'symfony_env' => [
+              'helper' => 'prod',
+              'label' => 'Environment',
           ],
-          "use_sudo" =>  [
-              "helper" => "false",
-              "label" => "Use sudo"
+          'use_sudo' => [
+              'helper' => 'false',
+              'label' => 'Use sudo',
           ],
-          "use_set_permissions" =>  [
-              "helper" => "true",
-              "label" => "Set permissions"
+          'use_set_permissions' => [
+              'helper' => 'true',
+              'label' => 'Set permissions',
           ],
-          "permission_method" =>  [
-              "helper" => ":chmod",
-              "label" => "Permission method",
-              "autocomplete" => [
-                  ":chmod", ":acl"
+          'permission_method' => [
+              'helper' => ':chmod',
+              'label' => 'Permission method',
+              'autocomplete' => [
+                  ':chmod', ':acl',
               ],
           ],
-          "keep_releases" =>  [
-              "helper" => 3,
-              "label" => "Number of releases"
+          'keep_releases' => [
+              'helper' => 3,
+              'label' => 'Number of releases',
           ],
-      ];
-      if (false === $fs->exists($deployRb)) {
-          $fs->touch($deployRb);
-      }
-      foreach ($properties as $key => $property) {
-          if ("deploy_to" == $key && null !== $data['ssh_user']) {
-              $property['helper'] = "/home/{$data['ssh_user']}/public_html";
-          }
-          $question = new Question("<info>{$property['label']}</info> [<comment>{$property['helper']}</comment>]: ", $property['helper']);
-          if (isset($property['autocomplete'])) {
-              $question->setAutocompleterValues($property['autocomplete']);
-          }
-          $data[$key] = $questionHelper->ask($input, $output, $question);
-      }
-      foreach ($data as $k => $v) {
-          if (!is_bool($v) && !is_int($v)) {
-              if (in_array($v, ['true', 'false'])) {
-                  $expression = "set :{$k}, {$v}".PHP_EOL;
-              } else {
-                  $expression = "set :{$k}, '{$v}'".PHP_EOL;
-              }
-          } else {
-              $expression = "set :{$k}, {$v}".PHP_EOL;
-          }
-          file_put_contents($deployRb, $expression, FILE_APPEND);
-      }
-      $output->writeln(['', " > generating <comment>{$appName}/config/deploy.rb</comment>"]);
-      $output->writeln(["<info>Successfully created.</info>", '']);
-      $output->writeln([$formatter->formatSection('PRODUCTION', 'Remote server / SSH settings'), '']);
-      $stagingPath = $root."/../config/deploy/production.rb";
-      $currentUser = get_current_user();
-      $currentOs = php_uname('s');
-      $serverOptions = [
-          "domain" => [
-              "helper" => "example.fr",
-              "label" => "Domain name"
+        ];
+        if (false === $fs->exists($deployRb)) {
+            $fs->touch($deployRb);
+        }
+        foreach ($properties as $key => $property) {
+            if ('deploy_to' == $key && null !== $data['ssh_user']) {
+                $property['helper'] = "/home/{$data['ssh_user']}/public_html";
+            }
+            $question = new Question("<info>{$property['label']}</info> [<comment>{$property['helper']}</comment>]: ", $property['helper']);
+            if (isset($property['autocomplete'])) {
+                $question->setAutocompleterValues($property['autocomplete']);
+            }
+            $data[$key] = $questionHelper->ask($input, $output, $question);
+        }
+        foreach ($data as $k => $v) {
+            if (!is_bool($v) && !is_int($v)) {
+                if (in_array($v, ['true', 'false'])) {
+                    $expression = "set :{$k}, {$v}".PHP_EOL;
+                } else {
+                    $expression = "set :{$k}, '{$v}'".PHP_EOL;
+                }
+            } else {
+                $expression = "set :{$k}, {$v}".PHP_EOL;
+            }
+            file_put_contents($deployRb, $expression, FILE_APPEND);
+        }
+        $output->writeln(['', " > generating <comment>{$appName}/config/deploy.rb</comment>"]);
+        $output->writeln(['<info>Successfully created.</info>', '']);
+        $output->writeln([$formatter->formatSection('PRODUCTION', 'Remote server / SSH settings'), '']);
+        $stagingPath = $root.'/../config/deploy/production.rb';
+        $currentUser = get_current_user();
+        $currentOs = php_uname('s');
+        $serverOptions = [
+          'domain' => [
+              'helper' => 'example.fr',
+              'label' => 'Domain name',
           ],
-      ];
-      $question = new Question("<info>{$serverOptions['domain']['label']}</info> [<comment>{$serverOptions['domain']['helper']}</comment>]: ", $serverOptions['domain']['helper']);
-      $sshProps["domain"] = $questionHelper->ask($input, $output, $question);
-      $expression = PHP_EOL."server '{$sshProps['domain']}',".PHP_EOL."user: '{$data["ssh_user"]}',".PHP_EOL;
-      $sshOptions = [
-          "forward_agent" => [
-              "label" => "SSH forward_agent",
-              "helper" => "false",
+        ];
+        $question = new Question("<info>{$serverOptions['domain']['label']}</info> [<comment>{$serverOptions['domain']['helper']}</comment>]: ", $serverOptions['domain']['helper']);
+        $sshProps['domain'] = $questionHelper->ask($input, $output, $question);
+        $expression = PHP_EOL."server '{$sshProps['domain']}',".PHP_EOL."user: '{$data['ssh_user']}',".PHP_EOL;
+        $sshOptions = [
+          'forward_agent' => [
+              'label' => 'SSH forward_agent',
+              'helper' => 'false',
           ],
-          "auth_methods" => [
-              "label" => "SSH auth method",
-              "helper" => "publickey password",
+          'auth_methods' => [
+              'label' => 'SSH auth method',
+              'helper' => 'publickey password',
           ],
-          "keys" => [
-              "label" => "Remote SSH key",
-              "helper" => ""
+          'keys' => [
+              'label' => 'Remote SSH key',
+              'helper' => '',
           ],
-      ];
-      $sshProps = [
-          "user" => $data['ssh_user'],
-          "keys" => "",
-          "forward_agent" => false,
-          "auth_methods" => "publickey password"
-      ];
-      if ($currentOs == 'Darwin') {
-          $sshOptions["keys"]["helper"] = "/Users/{$currentUser}/.ssh/id_rsa";
-      } elseif ($currentOs == 'Linux') {
-          $sshOptions["keys"]["helper"] = "/home/{$currentUser}/.ssh/id_rsa";
-      }
-      foreach ($sshOptions as $key => $property) {
-          $question = new Question("<info>{$property['label']}</info> [<comment>{$property['helper']}</comment>]: ", $property['helper']);
-          if (isset($property['autocomplete'])) {
-              $question->setAutocompleterValues($property['autocomplete']);
-          }
-          if (in_array($key, ["auth_methods", "keys"])) {
-              $sshProps[$key] = "%w(".$questionHelper->ask($input, $output, $question).")";
-          } else {
-              $sshProps[$key] = $questionHelper->ask($input, $output, $question);
-          }
-      }
-      $expression .= "ssh_options: {".PHP_EOL;
-      foreach ($sshProps as $k => $v) {
-          if ($k == "user") {
-              $expression .= "  {$k}: '{$v}',".PHP_EOL;
-          } else {
-              $expression .= "  {$k}: {$v},".PHP_EOL;
-          }
-      }
-      $expression .= "}";
-      file_put_contents($stagingPath, $expression);
+        ];
+        $sshProps = [
+          'user' => $data['ssh_user'],
+          'keys' => '',
+          'forward_agent' => false,
+          'auth_methods' => 'publickey password',
+        ];
+        if ($currentOs == 'Darwin') {
+            $sshOptions['keys']['helper'] = "/Users/{$currentUser}/.ssh/id_rsa";
+        } elseif ($currentOs == 'Linux') {
+            $sshOptions['keys']['helper'] = "/home/{$currentUser}/.ssh/id_rsa";
+        }
+        foreach ($sshOptions as $key => $property) {
+            $question = new Question("<info>{$property['label']}</info> [<comment>{$property['helper']}</comment>]: ", $property['helper']);
+            if (isset($property['autocomplete'])) {
+                $question->setAutocompleterValues($property['autocomplete']);
+            }
+            if (in_array($key, ['auth_methods', 'keys'])) {
+                $sshProps[$key] = '%w('.$questionHelper->ask($input, $output, $question).')';
+            } else {
+                $sshProps[$key] = $questionHelper->ask($input, $output, $question);
+            }
+        }
+        $expression .= 'ssh_options: {'.PHP_EOL;
+        foreach ($sshProps as $k => $v) {
+            if ($k == 'user') {
+                $expression .= "  {$k}: '{$v}',".PHP_EOL;
+            } else {
+                $expression .= "  {$k}: {$v},".PHP_EOL;
+            }
+        }
+        $expression .= '}';
+        file_put_contents($stagingPath, $expression);
 
-      $output->writeln("<comment>Remote server successfully configured</comment>");
-   }
+        $output->writeln('<comment>Remote server successfully configured</comment>');
+    }
 }
